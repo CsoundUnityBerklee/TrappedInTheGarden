72b26b70274476b8d46086ac2ea41491718bd483
mateolarreaferro <55174368+mateolarreaferro@users.noreply.github.com>
2022-10-04 21:05:17 -0400

    Sample Player / Sliders / Color Palette

---
+++ Trapped In The Garden/Assets/GravityMechanic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityMechanic : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<GravityZone>())
+        {
+            rb.useGravity = true;
+            rb.isKinematic = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<GravityZone>())
+        {
+            rb.useGravity = false;
+            rb.isKinematic = false;
+        }
+    }
+
+
+}
---
+++ Trapped In The Garden/Assets/SampleChanger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleChanger : MonoBehaviour
+{
+    // Changer Logic
+    [SerializeField] AudioSource _source;
+    [SerializeField] AudioClip[] _clips;
+    int idNumber;
+
+    // Sampler Maker
+    [SerializeField] GameObject[] sampleObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!_source.isPlaying)
+        {
+            if (other.gameObject.GetComponent<GravityMechanic>())
+            {
+                string objectName = other.gameObject.name;
+                int.TryParse(objectName, out idNumber);
+
+                _source.clip = _clips[idNumber];
+                _source.Play();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
