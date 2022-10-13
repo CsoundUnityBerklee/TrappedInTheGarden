eae27d7426770302894684d6dbd293a7901f1855
caiojmini <67804071+CaioMJ@users.noreply.github.com>
2022-10-12 21:19:58 -0400

    Updated Trapped in the Garden scene with my XR Origin and the trees prefabs

---
+++ Trapped In The Garden/Assets/Scripts/InteractionSwapper.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InteractionSwapper : MonoBehaviour
+{
+    public GetControllerButtonValues leftControllerValues, rightControllerValues;
+
+    [Header("HANDS AND DIRECT INTERACTOR")]
+    public GameObject handModel;
+    private XRDirectInteractor directInteractor;
+    [Header("RAY INTERACTOR")]
+    public GameObject handObject;
+    private 
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        directInteractor = GetComponent<XRDirectInteractor>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
