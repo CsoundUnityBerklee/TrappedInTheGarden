f71f57821410f4317a52e87261ef2e50801473c7
caiojmini <67804071+CaioMJ@users.noreply.github.com>
2022-10-18 23:05:20 -0400

    Fruits get transparent when you mute them with the B button

--- Trapped In The Garden/Assets/Scripts/Fruits/GetGrabbedFruitObject.cs
+++ Trapped In The Garden/Assets/Scripts/Fruits/GetGrabbedFruitObject.cs
@@ -12,6 +12,11 @@ public class GetGrabbedFruitObject : MonoBehaviour
     private bool canUpdateRotation = true;
     private bool rotationToggle = true;
 
+    //Visual Feedback
+    private Material fruitMaterial;
+    private Renderer fruitRenderer;
+    private Color fruitColor;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,12 +27,17 @@ public class GetGrabbedFruitObject : MonoBehaviour
     {
         csoundTransformSender = interactor.selectTarget.GetComponentInChildren<CsoundTransformAndPhysicsSender>();
         csoundUnity = interactor.selectTarget.GetComponentInChildren<CsoundUnity>();
+        fruitRenderer = interactor.selectTarget.GetComponent<Renderer>();
+        fruitMaterial = fruitRenderer.material;
+        fruitColor = fruitMaterial.color;
     }
 
     public void ResetReferences()
     {
         csoundTransformSender = null;
         csoundUnity = null;
+        fruitRenderer = null;
+        fruitMaterial = null;
     }
 
     public void UpdateCsoundPosition(bool update)
@@ -47,11 +57,13 @@ public class GetGrabbedFruitObject : MonoBehaviour
     public void VolumeGateOn()
     {
         StartCoroutine(InterpolateCsoundChannelValue.LerpChannelValue(csoundUnity, "masterLvl", 0.1f, 0, 0));
+        StartCoroutine(FadeAlpha(0.1f, 0, 0.25f));
     }
 
     public void VolumeGateOff()
     {
         StartCoroutine(InterpolateCsoundChannelValue.LerpChannelValue(csoundUnity, "masterLvl", 0.5f, 0, 1));
+        StartCoroutine(FadeAlpha(0.5f, 0, 1));
     }
 
     public void ToggleRotation()
@@ -70,5 +82,21 @@ public class GetGrabbedFruitObject : MonoBehaviour
             csoundTransformSender.UpdateRotation(false);
             rotationToggle = true;
         }
+    }
+
+    private IEnumerator FadeAlpha(float duration, float delay, float targetValue)
+    {
+        yield return new WaitForSeconds(delay);
+        float initialValue = fruitColor.a;
+        float currentTime = 0;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            fruitColor.a = Mathf.Lerp(initialValue, targetValue, currentTime / duration);
+            fruitRenderer.material.color = fruitColor;
+            yield return null;
+        }
+        yield break;
     }
 }
