77be65d8b45d0ac9cef22c708b10f423a89d54b6
caiojmini <67804071+CaioMJ@users.noreply.github.com>
2022-10-20 22:41:31 -0400

    Fixed teleportation

--- Trapped In The Garden/Assets/Scripts/Fruits/FruitParticles.cs
+++ Trapped In The Garden/Assets/Scripts/Fruits/FruitParticles.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FruitParticles : MonoBehaviour
 {
+    private ParticleSystem particles;
+    
     // Start is called before the first frame update
     void Start()
     {
