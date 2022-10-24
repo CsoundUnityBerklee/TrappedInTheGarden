247cd96a148ea664cc16cc4b66c81616c21dc4e8
mateolarreaferro <55174368+mateolarreaferro@users.noreply.github.com>
2022-10-24 01:04:59 -0400

    Implemented 'Stars'

---
+++ Trapped In The Garden/Assets/PlaySample.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySample : MonoBehaviour
+{
+    [SerializeField] AudioSource _source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<GravityZone>())
+            _source.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<GravityZone>())
+            _source.Stop();
+    }
+}
